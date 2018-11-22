fcf9dd832dd260a7bb066a9da54706151cf21d13
mckjulie <40745339+mckjulie@users.noreply.github.com>
2018-11-22 09:51:25 -0500

    Added JavaScript

---
+++ main.js
@@ -0,0 +1,38 @@
+﻿
+
+
+	
+	$(document).ready(function(){
+		
+		var panel = $('.panel').css('width');
+		
+		var width = panel.replace('px','');
+		var i = 0;
+		
+		$('ul li').each(function(){
+			i++;
+		});
+		
+		var val1 = width/i;
+		
+		$('.panel').css('width',val1 + 'px');
+		
+		$('ul li:nth-child(1)').on('click', function(){
+			$('.panel').animate({
+				left : '0px'
+			})
+		});
+		
+		$('ul li:nth-child(2)').on('click', function(){
+			$('.panel').animate({
+				left : val1 - 10 + 'px'
+			})
+		});
+		
+		$('ul li:nth-child(3)').on('click', function(){
+			$('.panel').animate({
+				left : val1 + val1 + 'px'
+			})
+		});
+	
+	});
